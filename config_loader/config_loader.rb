1717976f2af771c58a2b3fcd85e425780784d013
André Cardoso <andre.cardoso@3decide.com>
2013-02-05 00:37:15 +0000

    intermediate commit: some late ramblings

--- config_loader/config_loader.rb
+++ config_loader/config_loader.rb
@@ -1,13 +1,13 @@
 require 'yaml'
 require 'pathname'
-require 'torrent_item'
+#require 'torrent_item'
 
 class ConfigLoader
-    attr_accessor :file_path, :full_config
+    attr_accessor :file_path, :full_config, :torrent_list, :root_path
 
     def initialize(file_path)
         @file_path = File.join(Dir.getwd, file_path)
-        
+
         # make sure the file exists
         pn = Pathname.new(@file_path)        
         if not pn.exist?
@@ -20,7 +20,21 @@ class ConfigLoader
 
     def load_config()
         @full_config = YAML::load_file(@file_path)
-        puts @full_config.inspect
+
+        if @full_config['torrents'].empty?
+            raise 'The config file is incorrectly formed. A "torrents" part must exist!'
+        end
+
+        ## get hold of the interesting part
+        torrents = @full_config['torrents']
+
+        @root_path = torrents['rootPath'].to_s.empty? ? "/tmp/" : torrents['rootPath']
+
+
+        @full_config['torrents'].each_pair do |k,v|
+            puts k, "; ",  v
+        end
     end
+
 
 end
--- config_loader/torrent_item.rb
+++ config_loader/torrent_item.rb
@@ -1,6 +1,6 @@
 class TorrentItem
     attr_accessor :frequency, :url, :path, :last_checked
-    def initialize(url, path, frequency)
+    def initialize(url, path, frequency=0.5)
         @url = url
         @path = path
         @frequency = frequency
