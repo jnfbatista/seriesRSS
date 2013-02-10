c8c88857fa38d4fb749ea54e3a20c8044ef891a7
João Batista <jnfbatista@gmail.com>
2013-02-10 16:57:50 +0000

    Downloads torrents succesfully

--- seriesRSS.rb
+++ seriesRSS.rb
@@ -1,27 +1,34 @@
 #!/bin/env ruby
 
+require 'open-uri'
 
 # main function:
 # - gets the config (which is included in a 'require')
 # - dowloads the torrent 
 def main
-  file = '/tmp/shit'
 
-  f = File.open(file, 'w') #{ |file|  }
+  url = "http://torrents.thepiratebay.se/8125081/Star_Wars_The_Clone_Wars_S05E17_Sabotage_HDTV_x264-FQM.8125081.TPB.torrent"
+  folder = "/home/batista/"
+
+  f = File.open(file, 'w') 
+
   loop do 
-    f.puts "your text"
-    f.flush
-    puts "puts puts"
+    #f.puts "your text"
+    #f.flush
+    #puts "puts puts"
+    #
+    filename = url.split('/').last
+    
+    # Iterate the object list and dowload file
+    open(folder+filename, 'wb') do |file|
+        file << open(url).read
+    end
+
     sleep(5)
     # loads the file to the configuration object
 
   end
   f.close
-end
-
-# deamonize
-def daemonize 
-
 end
 
 # prints the usage 
