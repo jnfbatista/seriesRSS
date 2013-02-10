c9a7ebe9ff2564abc5658ed2a976b41f4f9bb772
João Batista <jnfbatista@gmail.com>
2013-02-10 19:15:16 +0000

    Parses RSS and lists torrent urls

--- config_loader/torrent_item.rb
+++ config_loader/torrent_item.rb
@@ -1,25 +1,40 @@
+require 'open-uri'
+require 'nokogiri'
+
 class TorrentItem
-    attr_accessor :id, :frequency, :url, :path, :last_checked
+  attr_accessor :id, :frequency, :url, :path, :last_checked
 
-    def initialize(id, url, path, frequency=0.5)
-        @id = id
-        @url = url
-        @path = path
-        @frequency = (frequency.nil?) ? 0.5 : frequency
-    end
-    #def initialize(h)
-    #    h.each {|k,v| send("#{k}=",v)}
-    #end
-    #
-    def get_torrent_url
+  def initialize(id, url, path, frequency=0.5)
+    @id = id
+    @url = url
+    @path = path
+    @frequency = (frequency.nil?) ? 0.5 : frequency
+  end
+  #def initialize(h)
+  #    h.each {|k,v| send("#{k}=",v)}
+  #end
+  #
 
+  def get_torrent_url
+    response = nil
+
+    while response.nil?
+      response = open(self.url)
     end
 
-    def to_s
-        ret = ""
-        ret += "Identifier: " + @id + "\n"
-        ret += "Location: " + @url + "\n"
-        ret += "Destination: " + @path + "\n"
-        ret += "Frequency: " + @frequency.to_s
+    xml = Nokogiri::XML(response)
+
+    xml.slop!()
+    xml.rss.channel.item.each do |it|
+      puts it.link.text
     end
+  end
+
+  def to_s
+    ret = ""
+    ret += "Identifier: " + @id + "\n"
+    ret += "Location: " + @url + "\n"
+    ret += "Destination: " + @path + "\n"
+    ret += "Frequency: " + @frequency.to_s
+  end
 end
--- seriesRSS.rb
+++ seriesRSS.rb
@@ -1,34 +1,40 @@
-#!/bin/env ruby
+#!/usr/bin/env ruby
 
 require 'open-uri'
+
+require_relative 'config_loader/config_loader'
 
 # main function:
 # - gets the config (which is included in a 'require')
 # - dowloads the torrent 
 def main
 
+  # Dummy data
   url = "http://torrents.thepiratebay.se/8125081/Star_Wars_The_Clone_Wars_S05E17_Sabotage_HDTV_x264-FQM.8125081.TPB.torrent"
   folder = "/home/batista/"
+  frequency = 60
 
-  f = File.open(file, 'w') 
+  config_loader = ConfigLoader.new ARGV[0] 
 
   loop do 
-    #f.puts "your text"
-    #f.flush
-    #puts "puts puts"
-    #
+    # for each series get the torrents
+    config_loader.torrent_list.each do |serie|
+      puts "Downloading #{serie.id}"
+      serie.get_torrent_url
+    end
+
+    # gets the filename
     filename = url.split('/').last
-    
+
     # Iterate the object list and dowload file
     open(folder+filename, 'wb') do |file|
-        file << open(url).read
+      file << open(url).read
     end
 
     sleep(5)
     # loads the file to the configuration object
 
   end
-  f.close
 end
 
 # prints the usage 
