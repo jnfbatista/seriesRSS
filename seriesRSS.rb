7c6b74674528e27ac514be57cf55f8344120cbb1
André Cardoso <andre.cardoso@3decide.com>
2013-05-02 21:14:33 +0100

    we are now downloading torrents and saving them on disk :)

--- seriesRSS.rb
+++ seriesRSS.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 
 require 'open-uri'
+require 'net/http'
+require 'fileutils'
+
 require_relative 'config_loader/config_loader'
 
 # main function:
@@ -22,17 +25,14 @@ def main
   loop do 
     # for each series get the torrents
     config_loader.torrent_list.each do |serie|
-      puts "\nDownloading #{serie.id} (last checked on #{serie.last_checked}) "
+      puts "\nDownloading \"#{serie.id}\" (last checked on #{serie.last_checked}) "
       serie.get_next_episode do |episode| 
+
 
         # gets the filename
         filename = episode.split('/').last
         puts filename
-
-        # Iterate the object list and dowload file
-        #open(folder+filename, 'wb') do |file|
-        #  file << open(url).read
-        #end
+        get_serie(serie, episode, filename)
 
       end
 
@@ -50,7 +50,32 @@ def usage
 end
 
 # gets the file and stores it
-def get_series
+# TODO: try for a fixed number of time, in case the torrent download
+# fails
+def get_serie(serie, url, filename)
+  tries = 3
+  while tries > 0
+    begin
+      uri = URI.parse(URI.encode(url, "[]"))
+      puts "Trying to download the following torrent:\n#{uri}."
+      torrent_content = Net::HTTP.get(uri)
+      break
+    rescue
+      puts "Oops, seems that an error occurred while trying to download the torrent #{url}!\n"
+      tries -= 1
+      sleep(3)
+    end
+  end
+
+  return nil if torrent_content.nil?
+  puts "Downloaded succeeded! Now saving torrent file on disk."
+
+  if not File.directory?(serie.path)
+    FileUtils.mkdir_p serie.path
+  end
+  new_torrent_file = open(File.join(serie.path, filename), 'wb')
+  new_torrent_file << torrent_content
+  new_torrent_file.close()
 
 end
 
